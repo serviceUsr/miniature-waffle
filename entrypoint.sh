da4a44fa98f2774edd0e53cb1fb7c1815f943b4f
serviceUsr <72531658+serviceUsr@users.noreply.github.com>
2025-08-16 02:05:17 +0200

    fixup! fixup! fixup! WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -91,8 +91,7 @@ for profile in $PROFILE; do
 	endgroup
 
 	if [ "$RET" -ne 0 ]; then
-		echo "::error => building $profile failed"
-		echo_red   "=> Package check failed: $RET)"
+		echo "::error => building $profile failed: $RET"
 		exit "$RET"
 	fi
 done
